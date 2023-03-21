4079e434d04d3fca1a06a4d986fc820533df7df3
seonysun <csh99240@naver.com>
2023-03-21 16:14:32 +0900

    BOOT 0321 Thymeleaf

--- SpringBootJPAThymeleafProject2/src/main/java/com/sist/web/SpringBootJpaThymeleafProject2Application.java
+++ SpringBootJPAThymeleafProject2/src/main/java/com/sist/web/SpringBootJpaThymeleafProject2Application.java
@@ -3,7 +3,7 @@ package com.sist.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-@ComponentScan(basePackages = {"com.sist.web.controller","com.sist.web.entity","com.sist.web.dao"})
+@ComponentScan(basePackages = {"com.sist.web.controller","com.sist.web.rest","com.sist.web.entity","com.sist.web.dao"})
 @SpringBootApplication
 public class SpringBootJpaThymeleafProject2Application {
 
